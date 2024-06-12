3b9c706f4a5adc70d781c531af7d0fa2d2d26f94
paulkiwana <paulkiwana6@gmail.com>
2024-06-12 06:15:30 +0300

    more changes, game still in progress, figuring out what it is while getting used to even listener concepts

--- unfinishedRPG.js
+++ unfinishedRPG.js
@@ -1,18 +1,24 @@
 let points = 0;
 const scorediv = document.getElementById("scorediv")
 const door1 = document.getElementById("door1")
+const scoredivOperations = () => {scorediv.innerHTML = `${points}points`
+scorediv.style.width = "100px";
+scorediv.style.height ="50px";
+scorediv.style.backgroundColor = "black"
+scorediv.style.margin = "auto"
+scorediv.style.textAlign = "center"}
 
 door1.addEventListener("click",function instruction(){
     points += 2
-    door1.innerHTML = "You enter a room with two more doors points"
     door1.style.backgroundColor = "green"
 door1.style.textAlign = "center"
-scorediv.innerHTML = `${points}points`
-})
+scoredivOperations()
+},{once:true})
 
 const door2 = document.getElementById("door2")
 
 door2.addEventListener("click", function instruction2(){
+    points += 2
     door2.style.backgroundColor = "orange"
-
-})
+    scoredivOperations()
+},{once:true})
