9ed11cee4ca9566dcec8dbfaa865f7088a147aaf
paulkiwana <paulkiwana6@gmail.com>
2024-06-12 06:57:06 +0300

    added a start button,it was actually a lot of fun

--- unfinishedRPG.js
+++ unfinishedRPG.js
@@ -3,12 +3,15 @@ const scorediv = document.getElementById("scorediv")
 const door1 = document.getElementById("door1");
 const door2 = document.getElementById("door2");
 const door3 = document.getElementById("door3");
+const container = document.getElementById("container");
+const start = document.getElementById("start")
 const scoredivOperations = () => {scorediv.innerHTML = `${points}points`
 scorediv.style.width = "100px";
 scorediv.style.height ="50px";
 scorediv.style.backgroundColor = "black"
 scorediv.style.margin = "auto"
 scorediv.style.textAlign = "center"}
+
 
 door1.addEventListener("click",function instruction(){
     points += 2
@@ -30,3 +33,8 @@ door3.addEventListener("click", instruction3 = () => {
     door3.style.backgroundColor = "black" 
     scoredivOperations()
 },{once:true})
+
+start.addEventListener("click", ()=> {
+    container.style.display = "flex";
+    start.style.display = "none";
+},{once:true});
